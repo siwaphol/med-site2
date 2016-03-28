4577ae7d7a6ffaba51d0141854d5b5492c6f643e
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-28 19:15:49 +0700

    Add all

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -8,7 +8,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a href="#" class="topnav-icons fa fa-home w3-left" title="Home"></a>
           </div>
           <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
             <ul class="nav navbar-nav">
