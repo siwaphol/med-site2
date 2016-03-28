19e6154d9200d52ec382d3305e1c23b2c94c8394
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-28 17:29:32 +0700

    Add all

--- resources/views/partials/_sidemenu.blade.php
+++ resources/views/partials/_sidemenu.blade.php
@@ -2,15 +2,36 @@
 <div class="col-md-2">
     <h4 style="font-size: 18px;">MICROBIOLOGY <!--Head-->
     </h4>
-    <ul class="nav nav-tabs nav-stacked">
+    <ul class="nav nav-tabs nav-stacked" aria-hidden="true">
         <li><a href="#">Home</a></li>
-        <li><a href="{{url('browse')}}">Faculty</a></li>
-        <li><a href="#">Department Faculty</a></li>
-        <li><a href="#">Administrative Staff</a></li>
+        <li><a href="{{url('browse')}}">Faculty</a>
+            <ul><a href="#">Faculty<a></ul>
+            <ul><a href="#">Staff</a></ul>
+            <ul><a href="#">Graduate Student</a></ul>
+            <ul><a href="#">Visiting Scholar & Postdocs</a></ul>
+        </li>
+        <li><a href="#">Curriculum</a>
+            <ul><a href="#">Undergraduate</a>
+                <li><a href="#">Medilicine</a></li>
+            </ul>
+            <ul><a href="#">Graduate</a>
+                <li><a href="#">Master of Science (M.Sc.)</a></li>
+                <li><a href="#">Doctor of Philosophy (Ph.D.)</a></li>
+                <li><a href="#">International Program</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Courses</a>
+            <ul><a href="#">Undergraduate</a>
+                <li><a href="#">Medicine</a></li>
+                <li><a href="#">Dentistry</a></li>
+                <li><a href="#">Pharmacy</a></li>
+                <li><a href="#">Pharmacy</a></li>
+                <li><a href="#">Nurse</a></li>
+            </ul>
+            <ul><a href="#">Graduate</a></ul>
+        </li>
         <li><a href="{{url("curriculum")}}">Curriculum</a></li>
         <li><a href="{{url("research")}}">Research</a></li>
-        <li><a href="#">Education & Training</a></li>
-        <li><a href="#">Seminars</a></li>
         <li><a href="#">Contact Us</a></li>
     </ul>
 
