5e5eadb5839c77999460cda2067963f970688f33
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-28 22:09:43 +0700

    Add all

--- resources/views/partials/_sidemenu.blade.php
+++ resources/views/partials/_sidemenu.blade.php
@@ -2,73 +2,36 @@
 <div class="col-md-2">
     <h4 style="font-size: 18px;">MICROBIOLOGY
     </h4>
-    <li class="hoe-has-menu opened">
-                            <a href="javascript:void(0)">
-                                <i class="fa fa-sitemap"></i>
-                                <span class="menu-text">Menu Lavel 1</span>
-                                <span class="selected"></span>
-                            </a>
-                            <ul class="hoe-sub-menu" style="display: block;">
-                                <li>
-                                    <a href="javascript:void(0)">
-                                        <span class="menu-text">level 2</span>
-                                        <span class="selected"></span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)">
-                                        <span class="menu-text">level 2</span>
-                                        <span class="selected"></span>
-                                    </a>
-                                </li>
-                                <li class="hoe-has-menu">
-                                    <a href="javascript:void(0)">
-                                        <span class="menu-text">level 2</span>
-                                        <span class="selected"></span>
-                                    </a>
-                                    <ul class="hoe-sub-menu">
-                                        <li class="hoe-has-menu">
-                                            <a href="javascript:void(0)">
-                                                <span class="menu-text">level 3</span>
-                                                <span class="selected"></span>
-                                            </a>
-                                            <ul class="hoe-sub-menu">
-                                                <li>
-                                                    <a href="javascript:void(0)">
-                                                        <span class="menu-text">level 4</span>
-                                                        <span class="selected"></span>
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href="javascript:void(0)">
-                                                        <span class="menu-text">level 4</span>
-                                                        <span class="selected"></span>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0)">
-                                                <span class="menu-text">level 3</span>
-                                                <span class="selected"></span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0)">
-                                                <span class="menu-text">level 3</span>
-                                                <span class="selected"></span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0)">
-                                                <span class="menu-text">level 3</span>
-                                                <span class="selected"></span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>  
-                            </ul>
-                        </li>
+    <ul class="nav navbar-nav">
+     <li class="active"><a href="#">Home</a></li>
+     <li><a href="#about">About</a></li>
+     <li><a href="#contact">Contact</a></li>
+     <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+      <ul class="dropdown-menu">
+       <li><a href="#">Action</a></li>
+       <li><a href="#">Another action</a></li>
+       <li><a href="#">Something else here</a></li>
+       <li class="divider"></li>
+       <li class="dropdown-header">Nav header</li>
+       <li><a href="#">Separated link</a></li>
+       <li><a href="#">One more separated link</a></li>
+       <li><a href="#">Action</a></li>
+       <li><a href="#">Another action</a></li>
+       <li><a href="#">Something else here</a></li>
+       <li class="divider"></li>
+       <li class="dropdown-header">Nav header</li>
+       <li><a href="#">Separated link</a></li>
+       <li><a href="#">One more separated link</a></li>
+       <li><a href="#">Action</a></li>
+       <li><a href="#">Another action</a></li>
+       <li><a href="#">Something else here</a></li>
+       <li class="divider"></li>
+       <li class="dropdown-header">Nav header</li>
+       <li><a href="#">Separated link test long title goes here</a></li>
+       <li><a href="#">One more separated link</a></li>
+      </ul>
+     </li>
+    </ul>
     <!--<ul class="nav nav-tabs nav-stacked">
         <li><a href="#">Home</a></li>
         <li>Faculty & Staff
