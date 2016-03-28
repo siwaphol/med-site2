ab6daa21ec68ba00860489d76dbfacf030053df3
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-29 01:02:02 +0700

    Add all

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default" style="background-color: #8e44ad;" >
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -10,11 +10,11 @@
           </div>
           <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
               <ul class="nav navbar-nav navbar-right ">
-                <ul class="nav navbar-nav">
-                    <li><a href="{{url("/")}}">Home</a></li>
+                <ul class="nav navbar-nav" >
+                    <li><a href="{{url("/")}}" style="color: #ffffff;">Home</a></li>
 
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Faculty & Staff<span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: #ffffff;">Faculty & Staff<span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="{{url('browse')}}">Staff</a></li>
                             <li><a href="#">Graduate Student</a></li>
@@ -22,7 +22,7 @@
                         </ul>
                     </li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Curriculum<span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: #ffffff;">Curriculum<span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li role="separator" class="divider"></li>
                             <li class="dropdown-header"><h4>Undergraduate</h4></li>
@@ -34,7 +34,7 @@
                         </ul>
                     </li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Courses<span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color: #ffffff;">Courses<span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li role="separator" class="divider"></li>
                             <li class="dropdown-header"><h4>Undergraduate</h4></li>
@@ -46,8 +46,8 @@
                         </ul>
                     </li>
 
-                    <li><a href="{{url("research")}}">Research</a></li>
-                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="{{url("research")}}" style="color: #ffffff;">Research</a></li>
+                    <li><a href="#" style="color: #ffffff;">Contact Us</a></li>
                 </ul>
             </ul>
           </div><!--/.nav-collapse -->
