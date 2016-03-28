26e9485fb1e0debef91ff3f12ac3c2d71b5715d1
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-28 18:54:06 +0700

    Add all

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <title>MICROBIOLOGY</title>
+  <link rel="stylesheet" href="{{asset("css/bootstrap.min.css")}}">
   <link rel="stylesheet" href="{{asset("css/custom-bootstrap.css")}}">
   <link rel="stylesheet" href="{{asset("css/custom.css")}}">
 
--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,5 +1,37 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
+
+
+<!--Odler-->
+
 <!-- Fixed navbar -->
-<nav class="navbar navbar-default navbar-fixed-top navbar-xs">
+<!--<nav class="navbar navbar-default navbar-fixed-top navbar-xs">
     <div class="container" >
         <div id="navbar" class="collapse navbar-collapse navbar-right">
             <ul class="nav navbar-nav">
@@ -23,5 +55,5 @@
             </ul>
         </div>
         <!--/.nav-collapse -->
-    </div>
+    <!--</div>
 </nav> <!-- end fixed navbar -->
