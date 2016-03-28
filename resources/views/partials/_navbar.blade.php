1614c3122962346b072ba7b2b7d0ed0ba6e1e993
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-28 19:24:51 +0700

    Add all

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse navbar-default navbar-fixed-top navbar-xs">
 
     <div class="container-fluid">
-          <div class="navbar-header navbar-fixed-top">
+          <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
@@ -10,7 +10,7 @@
                 </button>
           </div>
           <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
-            <ul class="nav navbar-nav navbar-right navbar-fixed-top">
+            <ul class="nav navbar-nav navbar-right">
                 <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
