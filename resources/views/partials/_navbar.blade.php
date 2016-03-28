bb99a731e9b5105ed8c25e62ade7f3446f17c099
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-28 20:37:21 +0700

    Add all

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse" style="background-color: #9b59b6;">
+<nav class="navbar navbar-inverse">
 
     <div class="container-fluid">
           <div class="navbar-header">
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                 </button>
           </div>
-          <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+          <div id="navbar" aria-expanded="false" style="height: 1px;">
             <ul class="nav navbar-nav navbar-right">
                 <ul class="nav navbar-nav">
                     <li><a href="{{url("/")}}">Home</a></li>
