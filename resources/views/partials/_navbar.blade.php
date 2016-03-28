bcde1469c72b6d489a116851d24e3e284bf212d4
Kittikawin <kittikawin_ball@hotmail.com>
2016-03-28 19:43:42 +0700

    Add all

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-default navbar-fixed-top navbar-xs">
 
     <div class="container-fluid">
           <div class="navbar-header">
@@ -12,21 +12,44 @@
           <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
             <ul class="nav navbar-nav navbar-right">
                 <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+                    <li class="active"><a href="{{url('ball2')}}">Home</a></li>
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Faculty & Staff<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{url('browse')}}">Staff</a></li>
+                            <li><a href="#">Graduate Student</a></li>
+                            <li><a href="#">Visiting Scholar & Postdocs</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Curriculum<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li role="separator" class="divider"></li>
+                            <li class="dropdown-header">Undergraduate</li>
+                                <li><a href="#">Medilicine</a></li>
+                            <li class="dropdown-header">Graduate</li>
+                                <li><a href="#">Master of Science (M.Sc.)</a></li>
+                                <li><a href="#">Doctor of Philosophy (Ph.D.)</a></li>
+                                <li><a href="#">International Program</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Courses<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li role="separator" class="divider"></li>
+                            <li class="dropdown-header">Undergraduate</li>
+                                <li><a href="#">Medicine</a></li>
+                                <li><a href="#">Dentistry</a></li>
+                                <li><a href="#">Pharmacy</a></li>
+                                <li><a href="#">Pharmacy</a></li>
+                                <li><a href="#">Nurse</a></li>
+                            <li class="dropdown-header">Graduate</li>
+                        </ul>
+                    </li>
+
+                    <li><a href="{{url("research")}}">Research</a></li>
+                    <li><a href="#">Contact Us</a></li>
                 </ul>
             </ul>
           </div><!--/.nav-collapse -->
