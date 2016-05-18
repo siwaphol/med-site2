16c4284cfbeb9c0d231fd0b7056ef5a35f512d2b
Kittikawin <kittikawin_ball@hotmail.com>
2016-05-18 14:55:27 +0700

    Add all

--- resources/views/frontend/courses.blade.php
+++ resources/views/frontend/courses.blade.php
@@ -1,9 +1,86 @@
 @extends("index")
-
+@section("content")
 <div class="row">
-  <div class="col-md-2"></div>
-  <div class="col-md-8 col-xs-12">
-  	Test
-  </div>
-  <div class="col-md-2"></div>
+  	<div class="col-md-10">
+
+  		<table class="table" style="background-color: #8e44ad; color: #ffffff;">
+    		<tr>
+      			<th><span style="font-weight:normal;">HOME &gt; </span>COURSES</th>
+    		</tr>
+  		</table>
+
+  		<div class = "row">
+  		<div class="col-md-12">
+  		<table class="table">
+  			<thead>
+    		<tr>
+      			<th>#</th>
+      			<th>รหัสวิชา</th>
+      			<th>รายชื่อกระบวนวิชา</th>
+    		</tr>
+  			</thead>
+  			<tbody>
+    			<tr>
+      				<th scope="row">1</th>
+      				<td>201110</td>
+      				<td>คณิตศาสตร์บูรณาการ (Integrated Mathematical Sciences )</td>
+    			</tr>
+    			<tr>
+      				<th scope="row">2</th>
+      				<td>201110</td>
+      				<td>คณิตศาสตร์บูรณาการ (Integrated Mathematical Sciences )</td>
+    			</tr>
+  			</tbody>
+		</table>
+		</div>
+  		</div>
+            <!--<div style=" height: 133px; width:730px; float: left; display:inline-block; margin:0;">
+
+            <table width="730" border="0" cellspacing="0" cellpadding="0">
+  			<tbody><tr>
+    			<td><img src="images/tabbanner/course.jpg"></td>
+  			</tr>
+  			<tr>
+    			<td class="path_head"><span class="path_regular">HOME &gt; </span>COURSES</td>
+  			</tr>
+			</tbody></table>
+
+			<table border="0" cellspacing="0" cellpadding="0" class="teacher_box">
+  			<tbody><tr>
+    			<td></td>
+  			</tr>
+  			<tr>
+    			<td style="margin-top:20px;">
+    			<table border="0" cellspacing="0" cellpadding="0">
+      			<tbody><tr>
+        			<td class="course_pic">
+        				<a href="index.php?name=course">
+        					<img src="images/pic_content/computer_science_course.png">
+        				</a>
+        			</td>
+        			<td class=" course_pic">
+        				<a href="index.php?name=course&file=homework">
+        					<img src="images/pic_content/computer_science_course.png">
+        				</a>
+        			</td>
+        			<td class="course_pic">
+        				<a href="http://www.cs.science.cmu.ac.th/person/wss/204399A/index.php?p=news" target="_blank">
+        					<img src="images/pic_content/computer_science_course.png">
+        				</a>
+        			</td>
+      			</tr>
+      			<tr>
+        			<td class="course_content">
+        				<a href="index.php?name=course">เว็บเพจของรายวิชาต่างๆ</a></td>
+        			<td class="course_content">
+        				<a href="index.php?name=course&file=homework">เว็บเพจส่งการบ้าน</a></td>
+        			<td class="course_content">
+        				<a href="http://www.cs.science.cmu.ac.th/person/wss/204399A/index.php?p=news" target="_blank">เว็บเพจการค้นคว้าอิสระ (IS)</a></td>
+      			</tr>
+    			</tbody>
+    			</table>
+  			</div>-->
+	<div class="col-md-2"></div>
+	</div>
 </div>
+@endsection
