b6f5fd4799a342ba1f1393aed8c4aefbe804c3ef
Kittikawin <kittikawin_ball@hotmail.com>
2016-05-23 22:09:18 +0700

    Add all

--- resources/views/partials/_sidemenu.blade.php
+++ resources/views/partials/_sidemenu.blade.php
@@ -155,3 +155,45 @@
     </li>  
 </ul>
 <!--End List-->
+
+
+<div class="nav-side-menu">
+    <!--<div class="brand">Brand Logo</div>-->
+        <div class="menu-list">
+  
+            <ul id="menu-content" class="menu-content collapse out">
+                <li>
+                  <a href="#">Dashboard</a>
+                </li>
+
+
+                <li data-toggle="collapse" data-target="#service" class="collapsed">
+                  <a href="#">Services <span class="arrow"></span></a>
+                </li>  
+                <ul class="sub-menu collapse" id="service">
+                  <li>New Service 1</li>
+                  <li>New Service 2</li>
+                  <li>New Service 3</li>
+                </ul>
+
+
+                <li data-toggle="collapse" data-target="#new" class="collapsed">
+                  <a href="#">New <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="new">
+                  <li>New New 1</li>
+                  <li>New New 2</li>
+                  <li>New New 3</li>
+                </ul>
+
+
+                <li>
+                  <a href="#">Profile</a>
+                </li>
+
+                <li>
+                  <a href="#">Users</a>
+                </li>
+            </ul>
+     </div>
+</div>
